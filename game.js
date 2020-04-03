4d6ed5d83a0f17e68c4d8b3ce36143c1ed1c7f8a
Jun Liang <illuminatemynight@gmail.com>
2020-04-03 12:55:59 -0500

    add questions

--- game.js
+++ game.js
@@ -1,8 +1,53 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-
+console.log(...choices)
 let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+
+let questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "<script>",
+        choice2: "<javascript>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer: 1
+    }
+]
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionsCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+        console.log(number,choice.dataset)
+        console.log(choice)
+    })
+
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+
+startGame();
